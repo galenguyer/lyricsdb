5043cb38eaef50c0c9536912f36c2b268557d346
Galen Guyer <galen@galenguyer.com>
2020-09-26 16:28:15 -0400

    Add azlyrics and genius base code

---
+++ lyricsdb/azlyrics.py
@@ -0,0 +1,134 @@
+"""
+lyric scraper for azlyrics.com
+"""
+
+import os
+import json
+import sys
+import re
+from json import JSONEncoder
+
+import requests
+from bs4 import BeautifulSoup
+
+
+class Song:
+    # pylint: disable=too-few-public-methods
+    """
+    song class used for json serialization
+    """
+    def __init__(self, title: str, artist: str, album: str, release: str, lyrics: str, url: str): # pylint: disable=too-many-arguments
+        self.title = title
+        self.artist = artist
+        self.album = album
+        self.release = release
+        self.lyrics = lyrics
+        self.url = url
+
+
+class SongEncoder(JSONEncoder):
+    # pylint: disable=too-few-public-methods
+    """
+    json encoder for song class
+    """
+    def default(self, o):
+        return o.__dict__
+
+
+class SearchResult:
+    # pylint: disable=too-few-public-methods
+    """
+    more basic song for selecting from search
+    """
+    def __init__(self, result):
+        self.link = result.a['href']
+        self.title = result.a.b.text.strip('" ')
+        self.artist = result.find_all('b')[1].text
+
+    def __str__(self):
+        return f'{self.title} by {self.artist}'
+
+
+def eprint(*args, **kwargs):
+    """
+    Print the given message to stderr
+    """
+    print(*args, file=sys.stderr, **kwargs)
+
+
+def search(term: str) -> str:
+    """
+    Search for a term
+    """
+    original_term = term
+    term = re.sub('[^a-zA-Z0-9 ]+', '', term).strip()
+    term = re.sub(' ', '+', term)
+    search_page = requests.get(f'https://search.azlyrics.com/search.php?q={term}&w=songs&p=1')
+    if search_page.status_code != 200:
+        eprint(f'Status code {search_page.status_code} for search term "{original_term}" ' + \
+            'indicates failure')
+        return None
+    parsed_page = BeautifulSoup(search_page.text, 'html.parser')
+    search_results = parsed_page.find_all('td', attrs={'class': 'text-left visitedlyr'})
+    results = [SearchResult(result) for result in search_results]
+    if len(results) == 0:
+        eprint(f'No songs found for query {original_term}')
+        sys.exit(1)
+    if len(results) == 1:
+        print(f'Only result found is {results[0]}')
+        return results[0].link
+    for num in range(1, min(16, len(results)+1)):
+        print(f'{num}. {results[num-1]}')
+    result = results[int(input('Select a number: '))-1]
+    return result.link
+
+
+def download_url(url: str):
+    """
+    Retrieve the page contents and parse out the lyrics from a given url
+    """
+    if not url.startswith('https://www.azlyrics.com/lyrics/'):
+        eprint(f'URL "{url}" does not appear to be a valid azlyrics url')
+        return None
+    result = requests.get(url)
+    if result.status_code != 200:
+        eprint(f'Status code {result.status_code} for url "{url}" indicates failure')
+        return None
+    parsed_page = BeautifulSoup(result.text, 'html.parser')
+    # lyrics are consistently on the 20th div in the page
+    lyrics = parsed_page.find_all('div', limit=21)[-1].text.strip()
+    artist = parsed_page.find_all('b')[0].text.strip().rsplit(' ', 1)[0]
+    song_title = parsed_page.find_all('b')[1].text.strip('" ')
+    album_info = parsed_page.find_all('div', attrs={'class': 'songinalbum_title'})[0]
+    album = album_info.b.text.strip('" ')
+    year = album_info.text.rsplit(' ', 1)[1].strip('( )')
+    return Song(title=song_title, artist=artist, album=album, release=year, lyrics=lyrics, url=url)
+
+
+def save_to_file(song: Song):
+    """
+    save a song to a file by song name and artist
+    """
+    filename = './lyrics/azlyrics_'
+    for char in song.title.lower():
+        if char.isalpha() or char.isdigit():
+            filename = filename + char
+        if char == ' ':
+            filename = filename + '-'
+    filename = filename + '_'
+    for char in song.artist.lower():
+        if char.isalpha() or char.isdigit():
+            filename = filename + char
+        if char == ' ':
+            filename = filename + '-'
+    filename = filename + '.json'
+    if not os.path.isdir('./lyrics'):
+        os.mkdir('./lyrics')
+    file = open(filename, 'w')
+    json.dump(song, file, indent=4, cls=SongEncoder)
+    file.close()
+    print('Lyrics saved to ' + filename)
+
+
+if __name__ == '__main__':
+    print('this is a module not a standalone app')
---
+++ lyricsdb/genius.py
@@ -0,0 +1,139 @@
+"""
+lyric scraper for genius.com
+"""
+
+import os
+import json
+import sys
+import re
+from json import JSONEncoder
+
+import requests
+from bs4 import BeautifulSoup
+
+
+class Song:
+    # pylint: disable=too-few-public-methods
+    """
+    song class used for json serialization
+    """
+    def __init__(self, title: str, artist: str, album: str, release: str, lyrics: str, url: str): # pylint: disable=too-many-arguments
+        self.title = title
+        self.artist = artist
+        self.album = album
+        self.release = release
+        self.lyrics = lyrics
+        self.url = url
+
+
+class SongEncoder(JSONEncoder):
+    # pylint: disable=too-few-public-methods
+    """
+    json encoder for song class
+    """
+    def default(self, o):
+        return o.__dict__
+
+
+class SearchResult:
+    # pylint: disable=too-few-public-methods
+    """
+    more basic song for selecting from search
+    """
+    def __init__(self, result):
+        result =  result['result']
+        self.link = result['url'].encode('ascii', 'ignore').decode('utf-8')
+        self.title = result['title'].encode('ascii', 'ignore').decode('utf-8')
+        self.artist = result['primary_artist']['name'].encode('ascii', 'ignore').decode('utf-8')
+
+    def __str__(self):
+        return f'{self.title} by {self.artist}'
+
+
+def eprint(*args, **kwargs):
+    """
+    Print the given message to stderr
+    """
+    print(*args, file=sys.stderr, **kwargs)
+
+
+def search(term: str) -> str:
+    """
+    Search for a term
+    """
+    original_term = term
+    term = re.sub('[^a-zA-Z0-9 ]+', '', term).strip()
+    term = re.sub(' ', '+', term)
+    search_page = requests.get(f'https://genius.com/api/search/song?page=1&q={term}')
+    if search_page.status_code != 200:
+        eprint(f'Status code {search_page.status_code} for search term "{original_term}" ' + \
+            'indicates failure')
+        return None
+    parsed_page = json.loads(search_page.text)
+    search_results = parsed_page['response']['sections'][0]['hits']
+    results = [SearchResult(result) for result in search_results]
+    if len(results) == 0:
+        eprint(f'No songs found for query {original_term}')
+        sys.exit(1)
+    if len(results) == 1:
+        print(f'Only result found is {results[0]}')
+        return results[0].link
+    for num in range(1, min(16, len(results)+1)):
+        print(f'{num}. {results[num-1]}')
+    result = results[int(input('Select a number: '))-1]
+    return result.link
+
+
+def download_url(url: str):
+    """
+    Retrieve the page contents and parse out the lyrics from a given url
+    """
+    if not url.startswith('https://genius.com/'):
+        eprint(f'URL "{url}" does not appear to be a valid genius lyrics url')
+        return None
+    result = requests.get(url)
+    if result.status_code != 200:
+        eprint(f'Status code {result.status_code} for url "{url}" indicates failure')
+        return None
+    parsed_page = BeautifulSoup(result.text.replace(u'\u2018', '\'').replace(u'\u2019', '\''),
+        'html.parser')
+    song_lyrics = parsed_page.find_all('div', attrs={'class': 'lyrics'})[0].text.strip()
+    song_data = json.loads([line for line in result.text.split('\n') if 'TRACKING_DATA' in line]\
+        [0].split('=')[1].strip(' ;'))
+    song_artist = song_data['Primary Artist'].encode('ascii', 'ignore').decode('utf-8')
+    song_title = song_data['Title'].encode('ascii', 'ignore').decode('utf-8')
+    song_album = (song_data['Primary Album'] if song_data['Primary Album'] is not None \
+        else 'Unknown Album').encode('ascii', 'ignore').decode('utf-8')
+    song_release = song_data['Release Date'].encode('ascii', 'ignore').decode('utf-8')
+    song = Song(title=song_title, artist=song_artist, album=song_album, \
+        lyrics=song_lyrics, url=url, release=song_release)
+    return song
+
+
+def save_to_file(song: Song):
+    """
+    save a song to a file by song name and artist
+    """
+    filename = './lyrics/genius_'
+    for char in song.title.lower():
+        if char.isalpha() or char.isdigit():
+            filename = filename + char
+        if char == ' ':
+            filename = filename + '-'
+    filename = filename + '_'
+    for char in song.artist.lower():
+        if char.isalpha() or char.isdigit():
+            filename = filename + char
+        if char == ' ':
+            filename = filename + '-'
+    filename = filename + '.json'
+    if not os.path.isdir('./lyrics'):
+        os.mkdir('./lyrics')
+    file = open(filename, 'w')
+    json.dump(song, file, indent=4, cls=SongEncoder)
+    file.close()
+    print('Lyrics saved to ' + filename)
+
+
+if __name__ == '__main__':
+    print('this is a module not a standalone app')
